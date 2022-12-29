7641ac094341ed342b24a399127e5a30abdb5ca2
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-29 14:19:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int sayi;
+int basamak_sayisi=0;
+
+Console.Write("Bir sayi giriniz: ");
+sayi = Convert.ToInt32(Console.ReadLine());
+
+
+    if (sayi >= 100)
+    {
+
+        Console.WriteLine("0-99.999 arasında bir sayı seçiniz ");
+
+    }
+    else
+    {
+        while (sayi > 0 && sayi < 99.999) {
+            sayi = sayi / 10;
+            basamak_sayisi++;
+
+        }
+        Console.WriteLine("Girdiginiz sayi " + basamak_sayisi + " basamaklıdır.");
+    }
+
+
+
+
+
+//while (sayi > 0  && sayi<99.999)
+//{//
+// sayi =sayi/10;
+//basamak_sayisi++;
+//}
+//Console.WriteLine("Girdiginiz sayi " + basamak_sayisi + " basamaklıdır.");
+
+//Console.ReadLine();
